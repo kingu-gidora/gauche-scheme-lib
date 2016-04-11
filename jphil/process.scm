ab14f1f8431f0332a3583abd6c6f21324a0e95f8
kingu-gidora <jphiltheberge@gmail.com>
2016-04-11 08:45:50 -0400

    Added process.scm

---
+++ jphil/process.scm
@@ -0,0 +1,20 @@
+(define-module jphil.process
+  (use gauche.process)
+  (export process->list process->string))
+(select-module jphil.process)
+
+(define %read-from-port
+  (lambda (p)
+    (let f ((x (read-line p)))
+      (if (eof-object? x) '() (cons x (f (read-line p)))))))
+
+(define process->list
+  (lambda (cmd)
+    (call-with-input-process
+     cmd
+     (lambda (p) (%read-from-port p))
+     :error :ignore )))
+
+(define process->string (lambda (c) (string-join (process->list c) "\n")))
+
+
