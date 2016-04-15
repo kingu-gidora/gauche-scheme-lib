5d1380c55685f08aee0cfbbc13dc5ec182bf5f75
jphil <jphil@proship.com>
2016-04-15 10:34:22 -0400

    Added sync.scm

---
+++ jphil/sync.scm
@@ -0,0 +1,18 @@
+(define-module jphil.sync
+  (use gauche.process)                                                                                 
+  (export sync sync-both-ways))
+(select-module jphil.sync)
+
+(define sync
+  (lambda (source target)
+    (unless (file-exists? source) (error "Source file does not exists!"))
+    (unless (file-exists? target) (error "Target file does not exists!"))
+    (let ((cmd `(rsync -rtu ,source ,target)))
+      (run-process cmd :wait #t))))
+
+(define sync-both-ways
+  (lambda (dir1 dir2)
+    (rsync dir1 dir2)
+    (rsync dir2 dir1)))
+
+
