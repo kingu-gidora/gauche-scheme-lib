896f2c8772e6f3a2da741e40963b6c23237bc59f
kingu-gidora <jphil@hellokitty.com>
2016-11-22 11:28:03 -0500

    oups last commit was wrong

--- jphil/time.scm
+++ jphil/time.scm
@@ -5,10 +5,11 @@
 
 (define-method decompose-seconds ((seconds <integer>) (format-string <string>))
   (let* ((sec (mod seconds 60))
-	 (min (round (div (mod total-seconds 3600) 60)))
-	 (hours (round (div (mod total-seconds 86400) 3600)))
-	 (days (round (div total-seconds (* 60 (* 60 24))))))
+	 (min (round (div (mod seconds 3600) 60)))
+	 (hours (round (div (mod seconds 86400) 3600)))
+	 (days (round (div seconds (* 60 (* 60 24))))))
     (format #f format-string days hours min sec))) 
 
 (define-method decompose-seconds ((seconds <integer>)) 
   (decompose-seconds seconds "~a days ~a hours ~a minutes & ~a seconds\n" ))
+x
