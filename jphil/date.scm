ff0ec30550c006b0c64ea221666131bc8bf05488
kingu-gidora <jphil@hellokitty.com>
2017-06-06 17:04:44 -0400

    Fixed previous day (was giving wrong results)

--- jphil/date.scm
+++ jphil/date.scm
@@ -3,32 +3,35 @@
   (export-all))
 (select-module jphil.date)
 
+
+(define is-leap-year? 
+  (lambda (year)
+    (if (or (and (zero? (mod year 4))
+		 (not (zero? (mod year 100))))
+	    (zero? (mod year 400)))
+	#t #f)))
+    
+(define day-in-month
+  (lambda (month year)
+    (~ (vector 31 (if (is-leap-year? year) 29 28) 31 30 31 30 31 31 30 31 30 31) (+ month 1))))
+
+
+
 ;; Check if the date is valid and had or will exist
 (define-method date-exists? ((date <date>))
-  (let ((M (date-month date))
-	(div? (lambda (x) (zero? (mod (date-year date) x)))))
-    (not (or (< M 1) (> M 12) 
-	     (> (date-day date) 
-		(~ (vector 31 
-			   (if (or (div? 400) (and (div? 4) (not (div? 100)))) 29 28)
-			   31 30 31 30 31 31 30 31 30 31)
-		   (- M 1)))))))
-
+  (not (or (< M 1) (> M 12) 
+	   (> (date-day date)
+	      (day-in-month (- (date-month date) 1) (date-year date))))))
+		
 (define-method previous-day ((date <date>))
   (let* ((y (date-year date))
 	 (m (date-month date))
-	 (d (date-day date))
-	 (d- (make-date 0 0 0 0 (- d 1) m y 0)))
-    (if (date-exists? d-) 
-	d-
-	(let ((m- (make-date 0 0 0 0 1 (- m 1) y 0)))
-	  (if (date-exists? m-)
-	      m-
-	      (let ((y- (make-date 0 0 0 0 1 1 (- y 1) 0)))
-		(if (date-exists? y-)
-		    y-
-		    (error "something is wrong!"))))))))
-  
+	 (d (date-day date)))
+    (if (> d 1)
+	(make-date 0 0 0 0 (- d 1) m y 0)
+	(if (> m 1)
+	    (make-date 0 0 0 0 (day-in-month (- m 1) y) (- m 1) y 0)
+	    (make-date 0 0 0 0 31 12 (- y 1))))))
 
 (define-method next-day ((date <date>))
   (let* ((y (date-year date))
